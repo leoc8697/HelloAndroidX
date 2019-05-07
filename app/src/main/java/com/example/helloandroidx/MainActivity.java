fe5511f90b0166789fe563b43e398d8ea4d6cad0
david <leoc@unicauca.edu.co>
2019-05-07 18:15:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloandroidx/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.helloandroidx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+        updateTime();
+    }
+
+    @Override
+    public void onClick(View v) {
+        updateTime();
+
+    }
+
+    private void updateTime() {
+        button.setText(new Date().toString());
+    }
+}
